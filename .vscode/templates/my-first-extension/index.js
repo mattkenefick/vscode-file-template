8b99a49b818f68fd133ad078e1425b27d03aca69
Matt Kenefick <keneficksays@gmail.com>
2023-11-13 13:38:54 -0500

    .

---
+++ .vscode/templates/my-first-extension/index.js
@@ -0,0 +1,17 @@
+// Hello world! ${package.author} is here!
+
+My Home: ${env.HOME}
+My User: ${env.USER}
+
+${package.name}
+${package.version}
+
+${{ workspaceRoot }}
+${{ inputPathRelative }}
+${{ outputPathRelative }}
+${{ inputDirectory }}
+${{ outputDirectory }}
+${{ inputDirectoryRelative }}
+${{ outputDirectoryRelative }}
+${{ inputFilename }}
+${{ outputFilename }}
